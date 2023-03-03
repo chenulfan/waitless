674878a2344010091dc20d423270cb61462ba2ce
Roybensira <Roybensira@gmail.com>
2023-03-03 18:19:27 +0200

    added functionality and styling to modal

--- src/shared/components/NewForm.jsx
+++ src/shared/components/NewForm.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Select from "react-select";
-import AsyncSelect from "react-select/async";
 import DatePicker from "react-datepicker";
 import { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 const NewForm = (props) => {
   const [startDate, setStartDate] = useState(new Date());
+  const [price, setPrice] = useState(0);
+  const [isSelling, setIsSelling] = useState(true);
+  const [restaurant, setRestaurant] = useState(undefined);
+
+  const handleSubmit = () => {
+    console.log("submitted");
+    console.log(isSelling);
+    console.log(restaurant);
+    console.log(startDate);
+    console.log(price);
+    alert("submitted");
+  };
+
+  const handleToggleAsk = () => {
+    setIsSelling(false);
+  };
+
+  const handleToggleSell = () => {
+    setIsSelling(true);
+  };
 
   const restaurantOptions = [
     { value: "ocd", label: "OCD" },
@@ -24,7 +43,14 @@ const NewForm = (props) => {
         aria-hidden="true"
         class="fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
       >
-        <div class="relative w-full h-full max-w-md md:h-auto">
+        <div
+          class="relative w-full h-full max-w-md md:h-auto"
+          style={{
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button
               type="button"
@@ -51,23 +77,81 @@ const NewForm = (props) => {
               <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
                 Wait Less!
               </h3>
-              <label
-                for="Toggle3"
-                class="inline-flex items-center p-2 rounded-md cursor-pointer dark:text-gray-800"
-              >
-                <input id="Toggle3" type="checkbox" class="hidden peer" />
-                <span class="px-4 py-2 rounded-l-md dark:bg-violet-400 peer-checked:dark:bg-gray-300">
-                  Sell a Reservation
-                </span>
-                <span class="px-4 py-2 rounded-r-md dark:bg-gray-300 peer-checked:dark:bg-violet-400">
-                  Ask For a Reservation
-                </span>
-              </label>
 
-              <form class="space-y-6" action="#">
+              <form class="space-y-6" onSubmit={handleSubmit}>
+                <ul class="grid w-full gap-6 md:grid-cols-2">
+                  <li>
+                    <input
+                      type="radio"
+                      id="hosting-small"
+                      name="hosting"
+                      value="hosting-small"
+                      class="hidden peer"
+                      require
+                      onClick={handleToggleSell}
+
+                    />
+                    <label
+                      for="hosting-small"
+                      class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                    >
+                      <div class="block">
+                        <div class="w-full text-lg font-semibold">
+                          Sell Reservation
+                        </div>
+                      </div>
+                      <svg
+                        aria-hidden="true"
+                        class="w-6 h-6 ml-3"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </label>
+                  </li>
+                  <li>
+                    <input
+                      type="radio"
+                      id="hosting-big"
+                      name="hosting"
+                      value="hosting-big"
+                      class="hidden peer"
+                      onClick={handleToggleAsk}
+                    />
+                    <label
+                      for="hosting-big"
+                      class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+                    >
+                      <div class="block">
+                        <div class="w-full text-lg font-semibold">
+                          Ask For a Reservation
+                        </div>
+                      </div>
+                      <svg
+                        aria-hidden="true"
+                        class="w-6 h-6 ml-3"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </label>
+                  </li>
+                </ul>
                 <div>
                   <label
-                    for="email"
+                    for="restaurant"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Choose Restaurant
@@ -76,12 +160,16 @@ const NewForm = (props) => {
                   <Select
                     isSearchable={true}
                     options={restaurantOptions}
-                    name="Restaurant"
+                    name="select"
+                    id="select"
+                    onChange={(value) => {
+                      setRestaurant(value.value);
+                    }}
                   />
                 </div>
                 <div>
                   <label
-                    for="password"
+                    for="date"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Select Date
@@ -91,46 +179,29 @@ const NewForm = (props) => {
                     onChange={(date) => setStartDate(date)}
                   />
                 </div>
-                <div class="flex justify-between">
-                  <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                      <input
-                        id="remember"
-                        type="checkbox"
-                        value=""
-                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                        required
-                      />
-                    </div>
-                    <label
-                      for="remember"
-                      class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                  <a
-                    href="#"
-                    class="text-sm text-blue-700 hover:underline dark:text-blue-500"
+                <div>
+                  <label
+                    for="date"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Lost Password?
-                  </a>
+                    Set Your Price
+                  </label>
+                  <input
+                    type="number"
+                    id="price"
+                    name="price"
+                    onChange={(event) => {
+                      setPrice(event.target.value);
+                    }}
+                  />
                 </div>
+
                 <button
                   type="submit"
                   class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Login to your account
+                  Submit
                 </button>
-                <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Not registered?{" "}
-                  <a
-                    href="#"
-                    class="text-blue-700 hover:underline dark:text-blue-500"
-                  >
-                    Create account
-                  </a>
-                </div>
               </form>
             </div>
           </div>
