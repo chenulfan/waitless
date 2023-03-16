51abf318fb64394cfa0d908f2330528a680e290f
Roybensira <Roybensira@gmail.com>
2023-03-16 14:26:08 +0200

    formatting

--- src/shared/components/card.jsx
+++ src/shared/components/card.jsx
@@ -1,16 +1,19 @@
 import Tag from "./tag";
 import axios from "axios";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Card = ({ img, imgAlt, title, content, tags }) => {
   const [imageUrl, setImageUrl] = useState("");
 
   const getRandomImageUrl = async () => {
     const response = await axios.get(
-      "https://api.pexels.com/v1/search?query="+tags.join("+")+"+restaurant+food&per_page=50",
+      "https://api.pexels.com/v1/search?query=" +
+        tags.join("+") +
+        "+restaurant+food&per_page=50",
       {
         headers: {
-          Authorization: "xRg2VlzNMpo4RaB50omLIecp9TPNLi1fRd0Ujf3ZiwubxYddk047XP4v",
+          Authorization:
+            "xRg2VlzNMpo4RaB50omLIecp9TPNLi1fRd0Ujf3ZiwubxYddk047XP4v",
         },
       }
     );
@@ -21,12 +24,15 @@ const Card = ({ img, imgAlt, title, content, tags }) => {
 
   useEffect(() => {
     getRandomImageUrl();
-  }, [])
-  
+  }, []);
 
   return (
     <div className="m-4 xl:w-1/5 lg:w-1/4 md:w-1/3 sm:w-1/2 rounded overflow-hidden shadow-lg">
-      <img className="w-full object-fit:cover h-80" src={imageUrl} alt={imgAlt} />
+      <img
+        className="w-full object-fit:cover h-80"
+        src={imageUrl}
+        alt={imgAlt}
+      />
 
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
